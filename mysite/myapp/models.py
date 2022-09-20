585b97aa0dd895b51c9cad97727dd9b3372b6110
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2022-09-20 12:22:29 +0300

    defined a __str__ method

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=100)
     price = models.IntegerField()
-    desc = models.CharField(max_length=1000)
+    desc = models.CharField(max_length=1)
